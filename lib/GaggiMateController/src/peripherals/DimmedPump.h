892dd3a3fbfbfb0026cd1254a26d9af51452231c
Jochen Ullrich <kontakt@ju-hh.de>
2025-03-17 04:32:47 +0100

    feat: Move everything into peripherals

---
+++ lib/GaggiMateController/src/peripherals/DimmedPump.h
@@ -0,0 +1,10 @@
+//
+// Created by Jochen Ullrich on 16.03.25.
+//
+
+#ifndef DIMMEDPUMP_H
+#define DIMMEDPUMP_H
+
+class DimmedPump {};
+
+#endif // DIMMEDPUMP_H
---
+++ lib/GaggiMateController/src/peripherals/PressureSensor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jochen Ullrich on 16.03.25.
+//
+
+#include "PressureSensor.h"
