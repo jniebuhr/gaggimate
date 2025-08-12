368710b23469e9898cdbd072fca81b04ea0edfe3
Borys Tymchenko <tim4bor@gmail.com>
2025-08-12 21:11:42 +0300

    Update settings to use 'pmc' key for pump model coefficients, fixing naming conflict

--- src/display/core/Settings.cpp
+++ src/display/core/Settings.cpp
@@ -18,7 +18,7 @@ Settings::Settings() {
     temperatureOffset = preferences.getInt("to", DEFAULT_TEMPERATURE_OFFSET);
     pressureScaling = preferences.getFloat("ps", DEFAULT_PRESSURE_SCALING);
     pid = preferences.getString("pid", DEFAULT_PID);
-    pumpModelCoeffs = preferences.getString("pm", DEFAULT_PUMP_MODEL_COEFFS);
+    pumpModelCoeffs = preferences.getString("pmc", DEFAULT_PUMP_MODEL_COEFFS);
     wifiSsid = preferences.getString("ws", "");
     wifiPassword = preferences.getString("wp", "");
     mdnsName = preferences.getString("mn", DEFAULT_MDNS_NAME);
@@ -356,7 +356,7 @@ void Settings::doSave() {
     preferences.putInt("to", temperatureOffset);
     preferences.putFloat("ps", pressureScaling);
     preferences.putString("pid", pid);
-    preferences.putString("pm", pumpModelCoeffs);
+    preferences.putString("pmc", pumpModelCoeffs);
     preferences.putString("ws", wifiSsid);
     preferences.putString("wp", wifiPassword);
     preferences.putString("mn", mdnsName);
