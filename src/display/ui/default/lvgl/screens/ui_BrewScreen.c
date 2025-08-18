b71ffe383efd4074ee169969b931bcd8a73ccc33
dragm83 <69442263+dragm83@users.noreply.github.com>
2025-08-18 19:51:18 -0400

    ui element adjustments

--- src/display/ui/default/lvgl/screens/ui_BrewScreen.c
+++ src/display/ui/default/lvgl/screens/ui_BrewScreen.c
@@ -392,12 +392,12 @@ void ui_BrewScreen_screen_init(void)
                                            _ui_theme_alpha_NiceWhite);
 
     ui_BrewScreen_brewTargetWeight = lv_label_create(ui_BrewScreen_Container2);
-    lv_obj_set_width(ui_BrewScreen_brewTargetWeight, 72);
+    lv_obj_set_width(ui_BrewScreen_brewTargetWeight, 90);
     lv_obj_set_height(ui_BrewScreen_brewTargetWeight, 30);
     lv_obj_set_x(ui_BrewScreen_brewTargetWeight, 104);
     lv_obj_set_y(ui_BrewScreen_brewTargetWeight, -120);
     lv_obj_set_align(ui_BrewScreen_brewTargetWeight, LV_ALIGN_CENTER);
-    lv_label_set_text(ui_BrewScreen_brewTargetWeight, "100g");
+    lv_label_set_text(ui_BrewScreen_brewTargetWeight, "100.5g");
     ui_object_set_themeable_style_property(ui_BrewScreen_brewTargetWeight, LV_PART_MAIN | LV_STATE_DEFAULT,
                                            LV_STYLE_TEXT_COLOR, _ui_theme_color_NiceWhite);
     ui_object_set_themeable_style_property(ui_BrewScreen_brewTargetWeight, LV_PART_MAIN | LV_STATE_DEFAULT,
