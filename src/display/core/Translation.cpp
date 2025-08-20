fc7ec6faf3b983a116f30cb5fd89b147829e25e7
Jake Beckerle <tekwiz123@gmail.com>
2025-08-20 07:19:50 -0700

    Add german

--- src/display/core/Translation.cpp
+++ src/display/core/Translation.cpp
@@ -69,24 +69,24 @@ const char* Translation::getEnglish(TranslationKey key) {
 
 const char* Translation::getGerman(TranslationKey key) {
     switch (key) {
-        case TranslationKey::BREW: return "Brew";
-        case TranslationKey::STEAM: return "Steam";
-        case TranslationKey::WATER: return "Water";
-        case TranslationKey::GRIND: return "Grind";
-        case TranslationKey::SELECT_PROFILE: return "Select profile";
-        case TranslationKey::STARTING: return "Starting...";
-        case TranslationKey::UPDATING: return "Updating...";
-        case TranslationKey::TEMPERATURE_ERROR: return "Temperature error, please restart";
-        case TranslationKey::AUTOTUNING: return "Autotuning...";
-        case TranslationKey::FINISHED: return "Finished";
+        case TranslationKey::BREW: return "Kaffee";
+        case TranslationKey::STEAM: return "Dampf";
+        case TranslationKey::WATER: return "Wasser";
+        case TranslationKey::GRIND: return "Mahlen";
+        case TranslationKey::SELECT_PROFILE: return "Profil auswählen";
+        case TranslationKey::STARTING: return "Starten...";
+        case TranslationKey::UPDATING: return "Aktualisieren...";
+        case TranslationKey::TEMPERATURE_ERROR: return "Temperaturfehler, bitte neu starten";
+        case TranslationKey::AUTOTUNING: return "Autotune...";
+        case TranslationKey::FINISHED: return "Fertig";
         case TranslationKey::INFUSION: return "INFUSION";
-        case TranslationKey::BREW_PHASE: return "BREW";
-        case TranslationKey::STEPS: return "Steps";
-        case TranslationKey::PHASES: return "Phases";
-        case TranslationKey::STEP: return "step";
-        case TranslationKey::PHASE: return "phase";
-        case TranslationKey::SELECTED_PROFILE: return "Selected profile";
-        case TranslationKey::RESTART_REQUIRED: return "Restart required";
+        case TranslationKey::BREW_PHASE: return "BEZUG";
+        case TranslationKey::STEPS: return "Schritte";
+        case TranslationKey::PHASES: return "Phasen";
+        case TranslationKey::STEP: return "Schritt";
+        case TranslationKey::PHASE: return "Phase";
+        case TranslationKey::SELECTED_PROFILE: return "Gewähltes Profil";
+        case TranslationKey::RESTART_REQUIRED: return "Neustart benötigt";
         default: return "";
     }
 }
