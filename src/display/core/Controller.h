b8b6cc33665a6711ca130a7e5edb35fb5bf57d42
Stephen <stephenwilley@users.noreply.github.com>
2026-03-09 15:55:36 -0700

    Add pressure offset using temperature offset as a template

--- src/display/core/Controller.h
+++ src/display/core/Controller.h
@@ -120,6 +120,7 @@ class Controller {
 
     // Event handlers
     void onTempRead(float temperature);
+    void onPressureRead(float pressure);
 
     // brew button
     void handleBrewButton(int brewButtonStatus);
