4b4a05b38b66883441d3f0f12022e1511a9d7a34
Tomáš Černík <Tom.cernik@gmail.com>
2026-06-16 22:11:10 +0200

    fix build

--- src/display/plugins/WebUIPlugin.cpp
+++ src/display/plugins/WebUIPlugin.cpp
@@ -601,7 +601,7 @@ void WebUIPlugin::handleProfileRequest(uint32_t clientId, JsonDocument &request)
     ws.text(clientId, buffer);
 }
 
-void WebUIPlugin::handleSettings(AsyncWebServerRequest *request) const {
+void WebUIPlugin::handleSettings(AsyncWebServerRequest *request) {
     if (request->method() == HTTP_POST) {
         controller->getSettings().batchUpdate([request](Settings *settings) {
             if (request->hasArg("startupMode"))
