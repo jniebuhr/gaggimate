07399cb7f52e5ac23051d5bc12e74c30c97219f8
Sartoric <6607379+Sartoric@users.noreply.github.com>
2026-07-20 17:40:29 +0200

    Add main menu event trigger and boiler refill logic
    - Triggered "ui:screen:menu" event on main menu load.
    - Added boiler refill logic when going from STEAM mode to the main menu, if startup fill time is zero,  to avoid starting with empty boiler.

--- src/display/plugins/BoilerFillPlugin.cpp
+++ src/display/plugins/BoilerFillPlugin.cpp
@@ -15,4 +15,11 @@ void BoilerFillPlugin::setup(Controller *controller, PluginManager *pluginManage
             this->controller->startProcess(new PumpProcess(this->controller->getSettings().getSteamFillTime()));
         }
     });
+    // Main Menu load event handler
+    pluginManager->on("ui:screen:menu", [this](Event const &) {
+        // If mode is STEAM and refill on startup is 0 start refill on main menu to avoid starting with empty boiler
+        if (this->controller->getSettings().getStartupFillTime() == 0 && this->controller->getMode() == MODE_STEAM) {
+             this->controller->startProcess(new PumpProcess(this->controller->getSettings().getSteamFillTime()));
+        }
+    });
 }
