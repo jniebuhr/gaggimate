c741e1b1cac9e7d17195c45d4f002d6ce93461a9
Jochen Ullrich <kontakt@ju-hh.de>
2026-08-22 22:13:22 +0200

    fix: Fix build

--- lib/NanoPbComm/src/GaggiMateClient.h
+++ lib/NanoPbComm/src/GaggiMateClient.h
@@ -16,7 +16,7 @@ class GaggiMateClient {
     using IncompatibleCallback = std::function<void(const String &info)>;
     using SystemInfoCallback =
         std::function<void(const char *hardware, const char *version, uint32_t protocolVersion, bool dimming, bool pressure,
-                           bool ledControl, bool tof, std::vector<uint32_t> addons)>;
+                           bool ledControl, bool tof, bool dualBoiler, std::vector<uint32_t> addons)>;
     using SensorCallback = std::function<void(float temperature, float temperature2, float pressure, float puckFlow,
                                               float pumpFlow, float puckResistance, float pumpPower, float heaterPower)>;
     using ButtonCallback = std::function<void(uint8_t index, bool pressed)>;
